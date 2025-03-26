b8ee1689dc891e4adb9f21e4d1c71433e5520c8c
Sathiel13 <ramirezsath@gmail.com>
2025-03-26 08:09:43 -0600

    Correccion Node.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("¡Hola desde CodePipeline!");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+module.exports = app;
